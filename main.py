4fd844902810cd78a9d975ceadc24421b3b4a183
mpco <aizaitianwai@gmail.com>
2018-09-11 20:44:20 +0800

    version 2.4 add support for Microsoft Office 2016 add debug method

--- main.py
+++ main.py
@@ -4,18 +4,18 @@
 # ~/Library/Application Support/com.apple.sharedfilelist/
 # https://www.mac4n6.com/blog/2017/10/17/script-update-for-macmrupy-v13-new-1013-sfl2-mru-files
 
-import ccl_bplist
-from mac_alias import Bookmark
-import sys
 import os
+import sys
 import json
 import time
+import plistlib
+import ccl_bplist
+from urllib import unquote
+from mac_alias import Bookmark
 
 
 def BLOBParser_human(blob):
-    # As described in:
     # http://mac-alias.readthedocs.io/en/latest/bookmark_fmt.html
-    # http://mac-alias.readthedocs.io/en/latest/alias_fmt.html
     try:
         b = Bookmark.from_bytes(blob)
         return "/" + u"/".join(b.get(0x1004, default=None))
@@ -25,7 +25,6 @@ def BLOBParser_human(blob):
 
 # for 10.13
 def ParseSFL2(MRUFile):
-
     itemsLinkList = []
     try:
         plistfile = open(MRUFile, "rb")
@@ -33,22 +32,18 @@ def ParseSFL2(MRUFile):
         plistfile.close()
         plist_objects = ccl_bplist.deserialise_NsKeyedArchiver(
             plist, parse_whole_structure=True)
-
         if plist_objects["root"]["NS.keys"][0] == "items":
             items = plist_objects["root"]["NS.objects"][0]["NS.objects"]
-
             for n, item in enumerate(items):
                 attribute_keys = plist_objects["root"]["NS.objects"][0]["NS.objects"][n]["NS.keys"]
                 attribute_values = plist_objects["root"]["NS.objects"][0]["NS.objects"][n]["NS.objects"]
                 attributes = dict(zip(attribute_keys, attribute_values))
-
                 if "Bookmark" in attributes:
-                    # print(type(attributes["Bookmark"]))
-                    # print(attributes["Bookmark"])
                     if isinstance(attributes["Bookmark"], str):
                         itemLink = BLOBParser_human(attributes["Bookmark"])
                     else:
-                        itemLink = BLOBParser_human(attributes["Bookmark"]['NS.data'])
+                        itemLink = BLOBParser_human(
+                            attributes["Bookmark"]['NS.data'])
                     itemsLinkList.append(itemLink)
             return itemsLinkList
     except Exception as e:
@@ -62,8 +57,8 @@ def ParseSFL(MRUFile):
         plistfile = open(MRUFile, "rb")
         plist = ccl_bplist.load(plistfile)
         plistfile.close()
-        plist_objects = ccl_bplist.deserialise_NsKeyedArchiver(plist, parse_whole_structure=True)
-
+        plist_objects = ccl_bplist.deserialise_NsKeyedArchiver(
+            plist, parse_whole_structure=True)
         if plist_objects["root"]["NS.keys"][2] == "items":
             items = plist_objects["root"]["NS.objects"][2]["NS.objects"]
             for n, item in enumerate(items):
@@ -85,11 +80,7 @@ def ParseFinderPlist(MRUFile):
         plistfile = open(MRUFile, "rb")
         plist = ccl_bplist.load(plistfile)
         plistfile.close()
-        # print "Parsing FXRecentFolders Key"
-        # print "[MRUs are listed from Newest to Oldest (ie: Item 0 - Item 9)]"
-        for n, item in enumerate(plist["FXRecentFolders"]):
-            # print "    [Item Number: " + str(n) + "] '" + item["name"] + "'"
-            # print item.keys()
+        for item in plist["FXRecentFolders"]:
             if "file-bookmark" in item:
                 blob = item["file-bookmark"]
             elif "file-data" in item:
@@ -112,13 +103,24 @@ def ParseSublimeText3Session(sessionFile):
     return itemsLinkList
 
 
+def ParseMSOffice2016Plist(MRUFile):
+    itemsLinkList = []
+    try:
+        plistfile = plistlib.readPlist(MRUFile)
+        for item in plistfile:
+            itemsLinkList.append(unquote(item[7:]).decode('utf8'))
+        return itemsLinkList
+    except Exception as e:
+        print e
+
+
 def checkFilesInExcludedFolders(fileList):
     folderListStr = os.environ["ExcludedFolders"]
     folderList = folderListStr.split(":")
     for folderPath in folderList:
         folderPath = os.path.expanduser(folderPath)
         if os.path.exists(folderPath) and os.path.isdir(folderPath):
-            # 为了防止 /xxx/xx 和 /xxx/xx x/xx 误判
+            # distinguish "/xxx/xx" and "/xxx/xx x/xx"
             if folderPath[-1] != "/":
                 folderPath += "/"
             for i, filePath in enumerate(fileList):
@@ -133,33 +135,42 @@ if __name__ == '__main__':
     allItemsLinkList = []
     for filePath in sys.argv[1:]:
         if filePath.endswith(".sfl2"):
+            if __debug__: print("#FileType: sfl2") # noqa
             itemsLinkList = ParseSFL2(filePath)
-        elif filePath.endswith("com.apple.finder.plist"):
-            itemsLinkList = ParseFinderPlist(filePath)
         elif filePath.endswith(".sfl"):
+            if __debug__: print("#FileType: sfl") # noqa
             itemsLinkList = ParseSFL(filePath)
+        elif filePath.endswith("com.apple.finder.plist"):
+            if __debug__: print("#FileType: com.apple.finder.plist") # noqa
+            itemsLinkList = ParseFinderPlist(filePath)
+        elif filePath.endswith(".securebookmarks.plist"):
+            if __debug__: print("#FileType: .securebookmarks.plist") # noqa
+            itemsLinkList = ParseMSOffice2016Plist(filePath)
         elif filePath.endswith(".sublime_session"):
+            if __debug__: print("#FileType: sublime_session") # noqa
             itemsLinkList = ParseSublimeText3Session(filePath)
         allItemsLinkList.extend(itemsLinkList)
     allItemsLinkList = checkFilesInExcludedFolders(allItemsLinkList)
 
     result = {"items": []}
     for item in allItemsLinkList:
-        # 滤除不存在的条目
+        # remove records of file not exist
         if not os.path.exists(item):
             continue
         modifiedTimeSecNum = os.path.getmtime(item)
-        modifiedTime = time.strftime("%d/%m %H:%M", time.localtime(modifiedTimeSecNum))
+        modifiedTime = time.strftime(
+            "%d/%m %H:%M", time.localtime(modifiedTimeSecNum))
         temp = {}
         temp["type"] = "file"
         temp["title"] = os.path.basename(item)
         temp["autocomplete"] = temp["title"]
         temp["icon"] = {"type": "fileicon", "path": item}
-        temp["subtitle"] = u"🕒 " + modifiedTime + u" 📡 " + item.replace(os.environ["HOME"], "~")
+        temp["subtitle"] = u"🕒 " + modifiedTime + \
+            u" 📡 " + item.replace(os.environ["HOME"], "~")
         temp["arg"] = item
         result['items'].append(temp)
     if result['items']:
         print json.dumps(result)
     else:
-        # 列表为空时
+        # when result list is empty
         print '{"items": [{"title": "None Recent Record","subtitle": "(*´･д･)?"}]}'
